9d6968352199a03df59774644bce2632af455c7b
michaluhrinek <uhrinekmichal@gmail.com>
2023-11-10 04:27:09 +0800

    Add files via upload
    Working with python and library Pandas(everything you need to know in one file)
        Loading Data:
            pd.read_csv(): Read data from a CSV file.
            pd.read_excel(): Read data from an Excel file.
            pd.read_sql(): Read data from a SQL database.
    
        Viewing Data:
            df.head(): View the first few rows of the DataFrame.
            df.tail(): View the last few rows of the DataFrame.
            df.info(): Display a concise summary of the DataFrame, including data types and missing values.
            df.describe(): Generate descriptive statistics of the DataFrame.
    
        Accessing and Manipulating Data:
            Accessing columns: df['column_name'] or df.column_name.
            Filtering rows: df[df['column'] > value].
            Selecting specific rows and columns: df.loc[] and df.iloc[].
            Adding a new column: df['new_column'] = ....
            Applying functions to columns: df['column'].apply(function).
    
        Data Cleaning:
            df.isnull(): Check for missing values.
            df.dropna(): Drop rows with missing values.
            df.fillna(value): Fill missing values with a specified value.
    
        Sorting:
            df.sort_values('column_name'): Sort DataFrame by values in a specific column.
            df.sort_index(): Sort DataFrame by index.
    
        Grouping and Aggregation:
            df.groupby('column_name').agg({'other_column': 'sum'}): Group by a column and perform aggregation.
    
        Statistical Operations:
            df.mean(), df.median(), df.std(): Calculate mean, median, and standard deviation.
    
        Saving and Writing Data:
            df.to_csv('file.csv', index=False): Save DataFrame to a CSV file.
            df.to_excel('file.xlsx', index=False): Save DataFrame to an Excel file.
            df.to_sql('table_name', connection_object): Write DataFrame to a SQL database.
    
        Creating DataFrames:
            Creating a DataFrame from a dictionary: pd.DataFrame({'col1': [...], 'col2': [...]}).
            Creating an empty DataFrame: pd.DataFrame().
    
        Merging and Concatenating:
            pd.concat([df1, df2]): Concatenate DataFrames.
            pd.merge(df1, df2, on='key'): Merge DataFrames on a specific column.
    
        Plotting:
            df.plot(): Plot the data in the DataFrame.
    Time Series Operations:
    
        Working with date and time data, resampling, and time-based indexing.
    Pivot Tables:
    
        Creating pivot tables to reshape and summarize data.
    Visualization:
    
        More advanced plotting options and customization using libraries like Matplotlib and Seaborn.
    
    Statistical Methods:
    
        Hypothesis testing, correlation, and other statistical methods.
    
    Integration with Other Libraries:
    
        Integration with machine learning libraries such as Scikit-learn for seamless data analysis and model building.

---
+++ SK_SPENDINGS.sql
@@ -0,0 +1,45 @@
+-- Create table
+CREATE TABLE SK_SPENDINGS
+(
+    date DATE PRIMARY KEY,
+    food_shopping DECIMAL(8, 2),
+    travel DECIMAL(8, 2),
+    miscellaneous DECIMAL(8, 2),
+    description CHAR(50),
+    sum_all DECIMAL(8, 2)
+);
+
+-- Insert data into table
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-10-25', 47, 0, 0, 'Some description', 47);
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-10-28', 13.85, 0, 0, 'Some description', 13.85);
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-10-30', 32.29, 0, 0, 'Some description', 32.29);
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-11-02', 42, 0, 0, 'Some description', 42);
+
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-11-03', 31, 0, 0, 'Some description', 31);
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-11-05', 5.97, 0, 0, 'Some description', 5.97);
+
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-11-06', 40, 3.60, 1.65, 'Some description', 41.65);
+
+INSERT INTO SK_SPENDINGS (date, food_shopping, travel, miscellaneous, description, sum_all)
+VALUES ('2023-11-08', 45, 0, 0, 'Some description', 45);
+
+
+SELECT *
+FROM SK_SPENDINGS;
+
+--SUM OF ITEMS 
+SELECT SUM(sum_all) 
+from SK_SPENDINGS;
